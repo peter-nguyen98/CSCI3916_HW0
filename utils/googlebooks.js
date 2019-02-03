79ec28fe215c176cce61cf5fbd316f8ffcfdbf11
peter-nguyen98 <46980992+peter-nguyen98@users.noreply.github.com>
2019-02-03 15:55:35 -0700

    second commit

--- utils/googlebooks.js
+++ utils/googlebooks.js
@@ -1,3 +1,4 @@
+// PETER
 const axios = require('axios')
 
 module.exports = async (phrase) => {
@@ -9,13 +10,22 @@ module.exports = async (phrase) => {
     })
 
     return JSON.stringify(results.data);
+
+    var returnVal = {
+        data: results.data,
+        status: results.status,
+        statusText: results.statusText,
+        headers: results.headers,
+        requestHeader: results.config.headers
+    };
+
+    return {returnVal};
 }
 
 /*  console.log(results.data);
     console.log(results.status);
     console.log(results.statusText);
     console.log(results.headers);
-
         const results = await axios({
         method: 'get',
         url: 'https://www.googleapis.com/books/v1/volumes',
